039b18f587b79e80063b8f7a0dd8fec1086464c4
ajnebro <ajnebro@users.noreply.github.com>
2024-04-16 09:47:52 +0200

    Refactor class Factorial

--- src/main/java/org/iis2024/factorial/Factorial.java
+++ src/main/java/org/iis2024/factorial/Factorial.java
@@ -4,14 +4,8 @@ public class Factorial {
 
   public long compute(long value) {
     long result ;
-    if ((value == 0) || (value == 1)) {
+    if (value == 0) {
       result = 1 ;
-    } else if (value == 2){
-      result = 2 ;
-    } else if (value == 3){
-      result = 6 ;
-    } else if (value == 4){
-      result = 24 ;
     } else {
       result = value * compute(value - 1) ;
     }
